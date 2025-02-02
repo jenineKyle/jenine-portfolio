5bfa8c81621303a4bbdc227af75211057011e7f3
Jenny <jennyrose.gutierrez02@gmail.com>
2025-02-01 23:43:26 -0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/next-js", // Change this to your GitHub repo name if using GitHub Pages
+  assetPrefix: "/next-js/",
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
